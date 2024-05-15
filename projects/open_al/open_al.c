9cfa6f1301dba49e7f78151384582aafd7fd7926
Nick L <nicklemmon@live.ca>
2024-05-15 15:56:04 -0400

    Using https://indiegamedev.net/2020/02/15/the-complete-guide-to-openal-with-c-part-1-playing-a-sound/ as a reference I was able to get sound working on windows at a basic level

--- projects/open_al/open_al.c
+++ projects/open_al/open_al.c
@@ -1,5 +1,7 @@
 #include "nl_lib.h"
 
+#include "open_al.h"
+
 #include "include/al.h"
 #include "include/alc.h"
 
@@ -7,7 +9,10 @@ ALCdevice *audio_device = {0};
 ALCcontext *audio_context = {0};
 
 #define MAX_SOUND_BUFFERS 3
-ALuint *sound_buffers = {0}
+ALuint sound_buffers[MAX_SOUND_BUFFERS] = {0};
+int _next_free_sound_in_buffer = 0;
+
+int song_sound = 0;
 
 static void print_audio_device_name(void)
 {
@@ -23,9 +28,80 @@ static void print_audio_device_name(void)
     NL_LOG("Opened %s", name);
 }
 
-static void load_sound(const char* filename)
+static ALuint load_sound(const char* filename)
 {
-    
+    if (_next_free_sound_in_buffer >= MAX_SOUND_BUFFERS-1)
+    {
+        NL_LOG("Unable to allocate more sounds for this buffer.  We have reached the max sounds and can't load %s", filename);
+        return 0;
+    }
+
+    file_contents sound_file = {0};
+    read_entire_file(filename, &sound_file);
+    if (sound_file.size == 0)
+    {
+        NL_LOG("Unable to open sound file: %s", filename);
+        return 0;
+    }
+
+    wav_file_header * const wav_header = (wav_file_header* const)(sound_file.content);
+    const short channels = wav_header->channels;
+    const short bits_per_sample = wav_header->bits_per_sample;
+    const int data_size = wav_header->data_size;
+    const int sample_rate = wav_header->sample_rate;
+
+    ALenum format = {0};
+    if (channels == 1 &&  bits_per_sample == 8)
+    {
+        format = AL_FORMAT_MONO8;
+    }
+    else if(channels == 1 && bits_per_sample == 16)
+    {   
+        format = AL_FORMAT_MONO16;
+    }
+    else if(channels == 2 && bits_per_sample == 8)
+    {
+        format = AL_FORMAT_STEREO8;
+    }
+    else if(channels == 2 && bits_per_sample == 16)
+    {
+        format = AL_FORMAT_STEREO16;
+    }   
+    else
+    {
+        NL_LOG("Unable to deduce the format of the audio file");
+    }
+
+    alBufferData(sound_buffers[_next_free_sound_in_buffer],
+                format, 
+                (const ALvoid*)(sound_file.content+sizeof(wav_file_header)),
+                data_size,
+                sample_rate
+                );
+
+    clear_file_read(&sound_file);
+
+    ALuint source;
+    alGenSources(1, &source);
+    alSourcef(source, AL_PITCH, 1);
+    alSourcef(source, AL_GAIN, 1.0f);
+    alSource3f(source, AL_POSITION, 0, 0, 0);
+    alSource3f(source, AL_VELOCITY, 0, 0, 0);
+    alSourcei(source, AL_LOOPING, AL_FALSE);
+    alSourcei(source, AL_BUFFER, sound_buffers[_next_free_sound_in_buffer]);
+
+    ++_next_free_sound_in_buffer;
+
+    alSourcePlay(source);
+
+    //ALint state = AL_PLAYING;
+    //while(state == AL_PLAYING)
+    //{
+    //    alGetSourcei(source, AL_SOURCE_STATE, &state);
+    //}
+    //alDeleteSources(1, &source);
+
+    return source;
 }
 
 void app_specific_init(void)
@@ -48,13 +124,30 @@ void app_specific_init(void)
     }
     print_audio_device_name();
 
+    alGenBuffers(MAX_SOUND_BUFFERS, sound_buffers);
+
+    song_sound = load_sound("data/StS_Funeral.wav");
 }
 
 void app_specific_update(double dt)
 {
+    if (was_key_pressed(key_space))
+    {
+        alSourcePlay(song_sound);
+    }
 }
 
 void app_specific_cleanup(void)
 {
     alDeleteBuffers(MAX_SOUND_BUFFERS, sound_buffers);
+
+    if (song_sound != 0)
+    {
+        alDeleteSources(1, &song_sound);
+    }
+
+    alcMakeContextCurrent(0);
+    alcDestroyContext(audio_context);
+
+    ALCboolean closed = alcCloseDevice(audio_device);
 }
