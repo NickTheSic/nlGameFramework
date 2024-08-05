5fc4d369b430138e70c26a57a5aa61f168c2f58f
Nick <nicklemmon@live.ca>
2024-08-05 00:14:00 +0000

    Created shell scripts for development on linux

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo please specify the platform
+fi
+
+if [ "$1" == "win" ]
+then
+    echo err not setup
+fi
+
+if [ "$1" == "web" ]
+then 
+    ./build_scripts/build_web.sh $2
+fi
+
+if [ "$1" == "tux" ]
+then
+    echo err not implemented
+fi
