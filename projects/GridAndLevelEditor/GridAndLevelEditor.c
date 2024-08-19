cba3ff636516544f8f904acad3b97acb5483aacc
Nick L <nicklemmon@live.ca>
2024-08-18 21:15:52 -0400

    Moving things into header files for better readability

--- projects/GridAndLevelEditor/GridAndLevelEditor.c
+++ projects/GridAndLevelEditor/GridAndLevelEditor.c
@@ -1,43 +1,7 @@
 #include "nl_lib.h"
 #include "private/nl_gl.h"
 
-typedef struct world_grid world_grid;
-struct  world_grid
-{
-    unsigned int width, height;
-    unsigned int tile_size;
-
-    unsigned int *tiles;
-};
-
-global_variable world_grid grid = {0};
-global_variable mesh tile_0 = {0};
-global_variable unsigned int shader_program = {0};
-
-unsigned int u_model_loc = 0;
-unsigned int u_view_mat = 0;
-camera main_cam = {0};
-
-static const char* vertex_shader_code =
-NL_SHADER_VERSION_HEADER
-"layout (location = 0) in vec3 aPos;                                   \n"
-"layout (location = 1) in vec4 aColor;                                 \n"
-"uniform mat4 uModelMat;                                               \n"
-"uniform mat4 uViewMat;                                                \n"
-"uniform mat4 uProjMat;                                                \n"
-"out vec4 oColor;                                                      \n"
-"void main() {                                                         \n"
-"   gl_Position = uProjMat * uViewMat * uModelMat * vec4(aPos,1.0);    \n"
-"   oColor = aColor;                                                   \n"
-"}                                                                     \0";
-
-static const char* fragment_shader_code =
-NL_SHADER_VERSION_HEADER
-"out vec4 FragColor;                                        \n"
-"in vec4 oColor;                                            \n"
-"void main() {                                              \n"
-"    FragColor = oColor;                                    \n"
-"}                                                          \0";
+#include "GridAndLevelEditor.h"
 
 v2i convert_index_to_grid_coords(world_grid *const grid, unsigned int index)
 {
