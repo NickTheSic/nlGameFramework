f9e050b2a8b98873baadaf1a74d5c5c5b98a2d4a
Nick L <nicklemmon@live.ca>
2024-11-13 19:58:00 -0500

    Rewokred the build c file

--- nl_lib/build_nl_lib.c
+++ nl_lib/build_nl_lib.c
@@ -1,49 +1,82 @@
 #include "nl_lib.h"
 
-
-/*-------Common Files--------------*/
-#include "private/common/nl_audio_openal.c"
-#include "private/common/nl_fileio.c"
-#include "private/common/nl_memory.c"
-#include "private/common/nl_math.c"
-#include "private/common/nl_camera.c"
-#include "private/common/nl_random.c"
-
-
-#if !defined(GEKKO)
-#include "private/common/nl_gamepad_null.c"
-
-#include "private/common/nl_renderer_gl_common.c"
-#include "private/common/nl_shader.c"
-#endif
-
-#include "private/common/nl_input.c"
-
-
-/*--------Third Party-------------*/
-#ifndef GEKKO
-#include "third_party/stb_image.c"
-#endif
+// This version specifically defines what is built for which platform
+// more copy paste, less awkward defines
 
 #if defined(_WIN32)
-#include "private/win32/nl_window_win32.c"
-#include "private/win32/nl_renderer_gl_win32.c"
-#include "private/win32/nl_gl_win32.c"
+#include "private/common/nl_audio_openal.c"
+#include "private/common/nl_camera.c"
+#include "private/common/nl_fileio.c"
+#include "private/common/nl_gamepad_null.c"
+#include "private/common/nl_input.c"
+#include "private/common/nl_math.c"
+#include "private/common/nl_memory.c"
+#include "private/common/nl_random.c"
+#include "private/common/nl_renderer_gl_common.c"
+#include "private/common/nl_shader.c"
+
+#include "third_party/stb_image.c"
+
 #include "private/win32/nl_deltatime_win32.c"
+#include "private/win32/nl_gl_win32.c"
+#include "private/win32/nl_renderer_gl_win32.c"
+#include "private/win32/nl_window_win32.c"
+
 
 #elif defined(__EMSCRIPTEN__)
+#include "private/common/nl_audio_openal.c"
+#include "private/common/nl_camera.c"
+#include "private/common/nl_fileio.c"
+#include "private/common/nl_gamepad_null.c"
+#include "private/common/nl_input.c"
+#include "private/common/nl_math.c"
+#include "private/common/nl_memory.c"
+#include "private/common/nl_random.c"
+#include "private/common/nl_renderer_gl_common.c"
+#include "private/common/nl_shader.c"
+
+#include "third_party/stb_image.c"
+
 #include "private/web/nl_window_web.c"
 #include "private/web/nl_renderer_gl_web.c"
 #include "private/web/nl_gl_web.c"
 #include "private/web/nl_deltatime_web.c"
 
+
 #elif defined(__linux__)
+#include "private/common/nl_audio_openal.c"
+#include "private/common/nl_camera.c"
+#include "private/common/nl_fileio.c"
+#include "private/common/nl_gamepad_null.c"
+#include "private/common/nl_input.c"
+#include "private/common/nl_math.c"
+#include "private/common/nl_memory.c"
+#include "private/common/nl_random.c"
+#include "private/common/nl_renderer_gl_common.c"
+#include "private/common/nl_shader.c"
+
+#include "third_party/stb_image.c"
+
 #include "private/linux/nl_window_linux.c"
 #include "private/linux/nl_renderer_gl_linux.c"
 #include "private/linux/nl_gl_linux.c"
 #include "private/linux/nl_deltatime_linux.c"
 
+
 #elif defined(GCN_GEKKO)
+//#include "private/common/nl_audio_openal.c"
+//#include "private/common/nl_renderer_gl_common.c"
+//#include "private/common/nl_shader.c"
+//#include "third_party/stb_image.c"
+
+#include "private/common/nl_camera.c"
+#include "private/common/nl_fileio.c"
+#include "private/common/nl_input.c"
+#include "private/common/nl_math.c"
+#include "private/common/nl_memory.c"
+#include "private/common/nl_random.c"
+
+#include "private/gcn/nl_renderer_gx_gcn.c"
 #include "private/gcn/nl_gamepad_gcn.c"
 #include "private/gcn/nl_window_gcn.c"
 
