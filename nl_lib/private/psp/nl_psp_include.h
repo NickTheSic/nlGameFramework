2913ca1c1f29f22e33e51ac43525ea1fe5533d7a
Nick L <nicklemmon@live.ca>
2024-11-20 14:18:26 -0500

    Added PSPMAIN THREAD to psp header for now

--- nl_lib/private/psp/nl_psp_include.h
+++ nl_lib/private/psp/nl_psp_include.h
@@ -9,6 +9,7 @@
 // TODO: Placing this here for testing purposes as this is PSP specific
 // in the future will consider moving this somewhere else to be changed per project
 PSP_MODULE_INFO("NL LIB", 0, 0, 1);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_VFPU | THREAD_ATTR_USER);
 
 #define BUFFER_WIDTH 512
 #define BUFFER_HEIGHT 272
