c1ee84a683d179a5ed1e424b1828ee8c1f55650f
Nick L <nicklemmon@live.ca>
2024-11-20 01:21:27 -0500

    Working psp build scrpt

--- build_scripts/build_psp.sh
+++ build_scripts/build_psp.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 PSP_SDK=$(psp-config -p)
 PSP_DEV=$(psp-config -d)
@@ -7,14 +7,18 @@ LIBDIR="-L$PSP_SDK/lib -L$PSP_DEV/lib -L$PSP_DEV/psp/lib"
 LIBS="-lpspdebug -lpspdisplay -lpspge"
 DEFINES="-DPSP -D_PSP_FW_VERSION=600 -D__PSP__"
 FLAGS="-Wl,-zmax-page-size=128"
-OUTPUT="_build/psp/$1/"
-mkdir "_build/psp/$1"
+OUTPUT="_build/psp"
 
-psp-gcc $DEFINES $INCLUDES $FLAGS -o $OUTPUT$1.c.obj -c $1/$1.c
+if [ ! -d "_build/psp/$1" ]
+then
+mkdir "_build/psp/$1"
+fi
+
+psp-gcc $DEFINES $INCLUDES $FLAGS -o $OUTPUT/$1.c.obj -c projects/$1/$1.c
 echo "code compiled"
-psp-gcc $INCLDUES -DPSP $LIBDIR $FLAGS $OUTPUT$1.c.obj -o $1 $LIBS
+psp-gcc $INCLDUES -DPSP $LIBDIR $FLAGS $OUTPUT/$1.c.obj -o $OUTPUT/$1.elf $LIBS
 echo "code linked"
-psp-fixup-imports $1
+psp-fixup-imports $OUTPUT/$1.elf
 echo "fixed imports"
-mksfoex -d MEMSIZE=1 -s APP_VER=01.00 $1 $OUTPUTPARAM.SFO
-pack-pbp $OUTPUTEBOOT.PBP PARAM.SFO NULL NULL NULL NULL NULL $OUTPUT$1 NULL
+mksfoex -d MEMSIZE=1 -s APP_VER=01.00 $1 $OUTPUT/PARAM.SFO
+pack-pbp $OUTPUT/$1/EBOOT.PBP $OUTPUT/PARAM.SFO NULL NULL NULL NULL NULL $OUTPUT/$1.elf NULL
