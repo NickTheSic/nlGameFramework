76a3dbd2662fc4ecc72e18087b13f059119a05ea
Nick L <nicklemmon@live.ca>
2024-11-23 00:51:04 -0500

    Added new sprites

--- projects/metroidvania/metroidvania.c
+++ projects/metroidvania/metroidvania.c
@@ -19,7 +19,7 @@ void app_specific_init(void)
     init_sprite_renderer();
 
     player_init(&player);
-    init_gameobject(&ground, 100, 20, (v2f){0.f,0.f}, "data/thing.png");
+    init_gameobject(&ground, 100, 20, (v2f){0.f,0.f}, "data/ground.png");
     init_gameobject(&mouse_follow, PLAYER_HALF_WIDTH, PLAYER_HALF_WIDTH, (v2f){0.f,0.f}, "data/test_sprite.png");
 
     pfn_window_size_callback = &winsizecbk;
