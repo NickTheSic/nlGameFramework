50400213df2fb7deee7da8ecda69d695a6ff6699
Nick L <nicklemmon@live.ca>
2024-12-15 01:34:18 -0500

    Cleaned up some linux build scripts as I mainly compile on windows at the moment.  Wanting to do some clean up and these are the easier things to clean up

--- build.sh
+++ build.sh
@@ -5,16 +5,6 @@ then
     echo please specify the platform
 fi
 
-if [ "$1" == "win" ]
-then
-    ./build_scripts/build_wintux_aio.sh nl_lib/build_nl_lib.c projects/sandbox/main.c projects/$2/$2.c
-fi
-
-if [ "$1" == "web" ]
-then 
-    ./build_scripts/build_web.sh $2
-fi
-
 if [ "$1" == "tux" ]
 then
     ./build_scripts/build_tux.sh $2
--- build_scripts/build_wintux_aio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-libs="-lUser32 -lopenGL -lgdi32 -lole32 -L/usr/x86_64-w64-mingw32/lib/"
-flags=
-includes="-I../../nl_lib -I/usr/x86_64-w64-mingw32/include/"
-
-output="_build/win/"
-
-gcc $flags $includes $1 $2 $3 $libs -o $output"build_nl_lib.exe"
