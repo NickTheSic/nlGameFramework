81cf40e4cc5b2b4c581ee414584721b7489f524f
Nick Lemmon <Nick@My-MacBook.lan>
2024-12-20 00:30:09 -0500

    Calling C functions from Objective C is way easier than I originally thought.  Wonder how I will call Obj-c from C

--- projects/main/mmain.m
+++ projects/main/mmain.m
@@ -1,11 +1,23 @@
+#include "nl_lib.h"
 #import <Foundation/Foundation.h>
+
+extern void app_specific_init(void);
+extern void app_specific_update(double dt);
+extern void app_specific_render(void);
+extern void app_specific_cleanup(void);
 
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     {
-        NSLog (@"hello world");
-        [pool drain];
+        NSLog (@"C include okay");
+       
+        app_specific_init();
+	app_specific_update(0.01);
+	app_specific_render();
+	app_specific_cleanup();
+
+	[pool drain];
     }
     return 0;
 }
