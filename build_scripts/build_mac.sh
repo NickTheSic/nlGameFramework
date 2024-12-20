03dea21cc58871ce693a3d447e1c5721d5556427
Nick Lemmon <Nick@My-MacBook.lan>
2024-12-20 00:08:25 -0500

    Added mac main function and linked objc and foundation framework

--- build_scripts/build_mac.sh
+++ build_scripts/build_mac.sh
@@ -4,7 +4,8 @@ INCLUDES="-Inl_lib/ -Iprojects/$1/"
 FLAGS="-g -v"
 COMPILER=clang
 OUTPUT="-o _build/mac/$1.a"
+LIBS="-lobjc -framework Foundation"
 
-NLBUILD="projects/main/main.c projects/$1/$1.c nl_lib/build_nl_lib.c"
+NLBUILD="projects/main/mmain.m projects/$1/$1.c nl_lib/build_nl_lib.c"
 
-$COMPILER $INCLUDES $FLAGS $NLBUILD
+$COMPILER $INCLUDES $FLAGS $NLBUILD $LIBS $OUTPUT
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ $1 == "mac" ]
+then
+    ./_build/mac/$2.a
+fi
