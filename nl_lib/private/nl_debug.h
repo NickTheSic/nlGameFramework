16d3edd45e5f9bda5f1d59ac904c06c27c5d4624
Nick L <nicklemmon@live.ca>
2024-12-28 18:29:13 -0500

    Removed unused debug defines

--- nl_lib/private/nl_debug.h
+++ nl_lib/private/nl_debug.h
@@ -9,6 +9,10 @@
 #   else
 #       define NL_LOG(m,...)
 #   endif
+#if defined(_WIN32)
+#   include <stdio.h>
+#   define NL_LOG(m,...) fprintf(stdout, m,##__VA_ARGS__); fprintf(stdout, "\n");
+#   endif
 #else
 #   include <stdio.h>
 #   define NL_LOG(m,...) fprintf(stdout, m,##__VA_ARGS__); fprintf(stdout, "\n");
@@ -26,29 +30,5 @@
 
 #define QUOTE_STR(name) #name // turn the value passed in into a string
 #define VALUE_TO_STR(name) QUOTE_STR(name) // Use this as for some reason the above doesn't work on its own
-
-#if defined(__EMSCRIPTEN__) || defined(GEKKO) || defined(PSP)
-#   define CANNOT_EXIT_MAIN_LOOP 1
-#else
-#   define CANNOT_EXIT_MAIN_LOOP 0
-#endif
-
-#if defined(GEKKO) || defined(PSP) || defined(__GBA__)
-#   define NOT_YET_IMPLEMENTED 1
-#else
-#   define NOT_YET_IMPLEMENTED 0
-#endif
-
-#if defined(__EMSCRIPTEN__) || defined(_WIN32)
-#   define NL_USE_GL_RENDERER 1
-#else
-#   define NL_USE_GL_RENDERER 0
-#endif
-
-#if defined(GEKKO) || defined(PSP) || defined(__GBA__) || defined(NDS)
-#   define PLATFORM_CONSOLE 1
-#else
-#   define PLATFORM_CONSOLE 0
-#endif
 
 #endif //__NL_DEBUG_H__
