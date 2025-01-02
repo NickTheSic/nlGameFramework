dfdebe5c53790c2ea6c031f2866928e26743e0bc
nickthesic <nickthesic@gmail.com>
2025-01-01 19:37:27 -0500

    Updated psp build to make folder

--- build_scripts/build_psp.sh
+++ build_scripts/build_psp.sh
@@ -10,7 +10,9 @@ DEFINES="-DPSP -D_PSP_FW_VERSION=600 -D__PSP__ $2"
 FLAGS="-Wall -Wextra -g -Wl,-zmax-page-size=128 -Wl,-Map,_build/psp/$1.map"
 PRX_SPECS="-specs=$PSP_SDK/lib/prxspecs -Wl,-q,-T$PSP_SDK/lib/linkfile.prx"
 OUTPUT="_build/psp"
- 
+
+mkdir -p $OUTPUT
+
 ICON0=res/icon0.png    #png Recommended: 144x80 
 ICON1=NULL              #pmf
 PIC0=NULL               #png
