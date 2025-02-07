12ebc9a2f86cd9bed623a77d5b24d7c94d45d728
NickTheSic <54951190+NickTheSic@users.noreply.github.com>
2025-02-07 11:24:52 -0500

    build-all.bat update

--- build-all.bat
+++ build-all.bat
@@ -1,3 +1,4 @@
-FOR /R "projects" %%i IN (*) DO (build.bat web %%~ni)
-
-::FOR /R "projects" %%i IN (*) DO (build.bat web %%~ni)
+FOR /R "projects" %%i IN (*) DO (
+    build.bat win %%~ni
+    build.bat web %%~ni
+    )
