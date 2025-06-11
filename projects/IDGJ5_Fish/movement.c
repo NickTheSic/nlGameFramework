8c0b394883beee4610c9a1ad68eaa56f2a11c9ed
NickTheSic <nicklemmon@live.ca>
2025-06-11 01:39:44 -0400

    fixed issues with the fish game I never made to see if I can keep or remove programmer art

--- projects/IDGJ5_Fish/movement.c
+++ projects/IDGJ5_Fish/movement.c
@@ -12,7 +12,6 @@ float get_movement_x()
 {
     float movement = 0;
 
-#if defined (__EMSCRIPTEN__) || defined (_WIN32)
     if (key_is_held(key_a))
     {
         movement -= 1.0f;
@@ -22,19 +21,18 @@ float get_movement_x()
     {
         movement += 1.0f;
     }
-#endif
-
-#if defined (PLATFORM_CONSOLE) || defined (_WIN32)
-    if (is_button_pressed(0, NL_GAMEPAD_DPAD_LEFT))
+    
+#if defined (_WIN32)
+    if (button_was_pressed(NL_GAMEPAD_DPAD_LEFT))
     {
         movement -= 1.0f;
     }
-    if (is_button_pressed(0, NL_GAMEPAD_DPAD_RIGHT))
+    if (button_was_pressed(NL_GAMEPAD_DPAD_RIGHT))
     {
         movement += 1.0f;
     }
 #endif
-
+    
     return movement;
 }
 
@@ -42,8 +40,6 @@ float get_movement_y()
 {
     float movement = 0;
     
-
-#if defined (__EMSCRIPTEN__) || defined (_WIN32)
     if (key_is_held(key_s))
     {
         movement -= 1.0f;
@@ -53,14 +49,13 @@ float get_movement_y()
     {
         movement += 1.0f;
     }
-#endif
 
-#if defined (PLATFORM_CONSOLE) || defined (_WIN32)
-    if (is_button_pressed(0, NL_GAMEPAD_DPAD_DOWN))
+#if defined (_WIN32)
+    if (button_was_pressed(NL_GAMEPAD_DPAD_DOWN))
     {
         movement -= 1.0f;
     }
-    if (is_button_pressed(0, NL_GAMEPAD_DPAD_UP))
+    if (button_was_pressed(NL_GAMEPAD_DPAD_UP))
     {
         movement += 1.0f;
     }
