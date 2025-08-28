092ae7b63f02cbbcaabef2920b2ac6da3c40f8ae
nickthesic <nickthesic@gmail.com>
2025-08-27 23:14:12 -0400

    Free sprites I created.  Still doesn't do much

--- projects/AnimatronicHorror/AnimatronicHorror.c
+++ projects/AnimatronicHorror/AnimatronicHorror.c
@@ -7,7 +7,7 @@ global_variable nl_sprite peanut_two = {0};
 
 void app_specific_init(void)
 {
-    //init_sprite_renderer();
+    init_sprite_renderer();
 
     load_texture_for_sprite(&peanut_one, "data/peanut_1.png");
     generate_rectangle_simple_sprite(&peanut_one, 32, 64);
@@ -23,13 +23,13 @@ void app_specific_update(double dt)
 void app_specific_render(void)
 {
     render_single_simple_sprite(&peanut_one);
-
     render_single_simple_sprite(&peanut_two);
 }
 
 void app_specific_cleanup(void)
 {
-    
+    free_simple_sprite(&peanut_two);
+    free_simple_sprite(&peanut_one);
 }
 
 #include "nl_sprite_renderer_gl.c"
