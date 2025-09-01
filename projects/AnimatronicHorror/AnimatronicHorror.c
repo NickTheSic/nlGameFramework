752b5aa4d0c30804be6ec2adf32c7232efa448f0
nickthesic <nickthesic@gmail.com>
2025-08-31 23:27:17 -0400

    Minor update to try rendering sprites

--- projects/AnimatronicHorror/AnimatronicHorror.c
+++ projects/AnimatronicHorror/AnimatronicHorror.c
@@ -5,6 +5,14 @@
 global_variable nl_sprite peanut_one = {0};
 global_variable nl_sprite peanut_two = {0};
 
+global_variable camera main_cam = {0};
+
+internal_function void winsizecbk(int width, int height)
+{
+    create_orthographic_projection(&main_cam.proj_matrix, 0, (float)width, 0, (float)height, -0.1f, 100.f);
+    set_projection_matrix(&main_cam.proj_matrix);
+}
+
 void app_specific_init(void)
 {
     init_sprite_renderer();
@@ -13,6 +21,13 @@ void app_specific_init(void)
     generate_rectangle_simple_sprite(&peanut_one, 32, 64);
     load_texture_for_sprite(&peanut_two, "data/peanut_2.png");
     generate_rectangle_simple_sprite(&peanut_two, 32, 64);
+
+    set_window_size_callback(&winsizecbk);
+    v2i screen_size = get_screen_size();
+    winsizecbk(screen_size.x, screen_size.y);
+
+    create_srt_matrix(&main_cam.view_matrix, (v3f){1.0f,1.0f,0.0f}, (v3f){0.0f,0.0f,0.0f}, (v3f){0.0f,0.0f,0.0f});
+    set_view_matrix(&main_cam.view_matrix);
 }
 
 void app_specific_update(double dt)
@@ -22,7 +37,17 @@ void app_specific_update(double dt)
 
 void app_specific_render(void)
 {
+    mat4x4f model = {0};
+    create_identity_matrix(&model);
+
+    model.m41 = 10.0f;
+    model.m42 = 10.0f;
+    set_model_matrix(&model);
     render_single_simple_sprite(&peanut_one);
+
+    model.m41 = 300.0f;
+    model.m42 = 300.0f;
+    set_model_matrix(&model);
     render_single_simple_sprite(&peanut_two);
 }
 
