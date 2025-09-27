4cbac50c93329ea2c571d9b7c9c077b54a563961
Nickthesic <nickthesic@gmail.com>
2025-09-26 20:44:49 -0400

    Minor non update to remove whitespace

--- build_scripts/build_wintux.sh
+++ build_scripts/build_wintux.sh
@@ -5,11 +5,8 @@ pushd "_build/win32/$1"
 
 
 libs="-luser32 -lopengl32 -lgdi32 -lole32 -lxaudio2_9"
-
 flags="-Wall -Wextra -g --static"
-
 includes="-I../../../nl_lib -I../../../projects/$1 -I/usr/x86_64-w64-mingw32/include"
-
 FILES="../../../nl_lib/build_nl_lib.c ../../../projects/$1/$1.c"
 
 x86_64-w64-mingw32-gcc $EXTRA_FLAGS_WIN $flags $includes $FILES $libs
