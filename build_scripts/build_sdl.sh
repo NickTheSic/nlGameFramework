d5213fe9f3efaa049620466f2a09506901fd05ba
Nickthesic <nickthesic@gmail.com>
2025-09-26 23:11:23 -0400

    Removed unused build scripts.  May skip on linux dev for right now and come back to it in the future

--- build_scripts/build_sdl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-output="_build/sdltux/$1"
-mkdir -p $output
-
-flags="-Wall -Wextra -g -lm"
-includes="-Inl_lib -Iprojects/$1"
-
-gcc $flags $includes nl_lib/build_nl_lib.c projects/$1/$1.c -o$output/$1
