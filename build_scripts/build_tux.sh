0217c984fa354168d07f318080cf33aa84e4d42a
Nickthesic <nickthesic@gmail.com>
2025-09-26 21:51:23 -0400

    Added gdb flag so I have debug info for when I get around to that

--- build_scripts/build_tux.sh
+++ build_scripts/build_tux.sh
@@ -3,7 +3,7 @@
 output="_build/tux/$1"
 mkdir -p $output
 
-flags="-Wall -Wextra -g -lm"
+flags="-Wall -Wextra -ggdb -lm"
 includes="-Inl_lib -Iprojects/$1"
 
 gcc $flags $includes nl_lib/build_nl_lib.c projects/$1/$1.c -o$output/$1
--- build_scripts/build_wintux.sh
+++ build_scripts/build_wintux.sh
@@ -5,7 +5,7 @@ pushd "_build/win32/$1"
 
 
 libs="-luser32 -lopengl32 -lgdi32 -lole32 -lxaudio2_9"
-flags="-Wall -Wextra -g --static"
+flags="-Wall -Wextra -ggdb --static"
 includes="-I../../../nl_lib -I../../../projects/$1 -I/usr/x86_64-w64-mingw32/include"
 FILES="../../../nl_lib/build_nl_lib.c ../../../projects/$1/$1.c"
 
