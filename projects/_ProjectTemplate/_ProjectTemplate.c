8b4b19c87bab777fb529a449b0d28a90aaa6a762
nick <nickthesic@gmail.com>
2026-02-28 00:01:47 -0500

    Now that I created testing I can remove debug stuff from the project template

--- projects/_ProjectTemplate/_ProjectTemplate.c
+++ projects/_ProjectTemplate/_ProjectTemplate.c
@@ -1,10 +1,8 @@
 #include "nl_lib.h"
 
-int *test_ptr = 0;
-
 void app_specific_init(void)
 {
-    test_ptr = memory_allocate(sizeof(int));
+    
 }
 
 void app_specific_update(double dt)
@@ -19,5 +17,5 @@ void app_specific_render(void)
 
 void app_specific_cleanup(void)
 {
-    memory_free(test_ptr);
+
 }
