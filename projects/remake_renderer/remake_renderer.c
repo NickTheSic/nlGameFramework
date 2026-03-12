7d9ccc1640f0a66e56994038726ff1177facfcce
NickTheSic <54951190+NickTheSic@users.noreply.github.com>
2026-03-12 14:13:19 -0400

    Started going through the opengl tutorial site again

--- projects/remake_renderer/remake_renderer.c
+++ projects/remake_renderer/remake_renderer.c
@@ -1,8 +1,28 @@
 #include "nl_lib.h"
+#include "private/gl/nl_gl.h"
+
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
+unsigned int VBO = 0;
+
+const char* rr_vertex_shader = 
+"#version 330 core\n" 
+"layout (location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
 
 void app_specific_init(void)
 {
-    
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
 void app_specific_update(double dt)
@@ -12,7 +32,7 @@ void app_specific_update(double dt)
 
 void app_specific_render(void)
 {
-
+    glClearColor(0.2f,0.3f,0.8f,1.0f);
 }
 
 void app_specific_cleanup(void)
