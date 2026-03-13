bd696fab47c761fca64cdaa8bdea27734feb9384
nick <nickthesic@gmail.com>
2026-03-13 01:34:50 -0400

    removed print logs and fixed the error that I added for testing

--- projects/remake_renderer/remake_renderer.c
+++ projects/remake_renderer/remake_renderer.c
@@ -12,7 +12,7 @@ unsigned int VBO = 0;
 
 const char* rr_vertex_shader_code = 
 "#version 330 core\n" 
-"layout (location = 0) in vec3 Pos;\n"
+"layout (location = 0) in vec3 aPos;\n"
 "void main() {\n"
 "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
@@ -22,11 +22,6 @@ unsigned int rr_vertex_shader_program = 0;
 
 void app_specific_init(void)
 {
-    NL_LOG("Application Initializing...");
-printf("stdout handle: %p\n", GetStdHandle(STD_OUTPUT_HANDLE));
-    fprintf(stdout, "TEST\n");
-    printf("TEst1");
-
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
