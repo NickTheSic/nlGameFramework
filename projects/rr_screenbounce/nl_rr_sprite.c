c4cae244d848f65f927dc97ea0f31feed6492d10
Nick <nick@local.lan>
2026-03-20 00:36:33 -0400

    wanted to make the dvd bounce screen saver but wasn't sure how to go about it

---
+++ projects/rr_screenbounce/nl_rr_sprite.c
@@ -0,0 +1,117 @@
+#include "nl_rr_sprite.h"
+#include "private/gl/nl_gl.h"
+#include "third_party/stb/stb_image.h"
+#include <string.h>
+
+global_variable nl_rr_vertex_data vertices[] = {
+    {{-1.0f, -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+    {{ 1.0f, -1.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+    {{ 1.0f,  1.0f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+    {{-1.0f,  1.0f, 0.0f}, {0.8f, 0.2f, 0.8f}, {0.0f, 1.0f}},
+};
+
+global_variable unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
+};
+
+
+void create_simple_rr_sprite(const char* filename, nl_rr_sprite* const rr_sprite)
+{
+    glGenVertexArrays(1, &rr_sprite->VAO);
+    glGenBuffers(1, &rr_sprite->VBO);
+    glGenBuffers(1, &rr_sprite->EBO);
+    
+    glBindVertexArray(rr_sprite->VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, rr_sprite->VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rr_sprite->EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Need to bind this items here.  each attribute takes is managed by a VBO
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(nl_rr_vertex_data), (void*)offsetof(nl_rr_vertex_data, position));
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(nl_rr_vertex_data), (void*)offsetof(nl_rr_vertex_data, colour));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(nl_rr_vertex_data), (void*)offsetof(nl_rr_vertex_data, uv));
+    glEnableVertexAttribArray(2);
+
+
+    glGenTextures(1, &rr_sprite->TextureID);
+    glBindTexture(GL_TEXTURE_2D, rr_sprite->TextureID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    
+    char path[64] = "data/images/";
+    strcat(path, filename);
+
+    stbi_set_flip_vertically_on_load(1);
+    int width, height, channels;
+    unsigned char* data = stbi_load(path, &width, &height, &channels, 0);
+
+    if (data)
+    {
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0, 
+            GL_RGBA, 
+            width, height, 
+            0, 
+            GL_RGBA, 
+            GL_UNSIGNED_BYTE, 
+            data
+        );
+    }
+    else
+    {
+        NL_LOG("Failed to load image with STBI");
+    }
+
+    stbi_image_free(data);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    rr_sprite->scale = (v3f){(float)width, (float)height, 0.0f};
+    rr_sprite->rotation = (v3f){0.0f,0.0f,0.0f};
+    rr_sprite->position = (v3f){0.0f,0.0f,0.0f};
+
+    create_srt_matrix(&rr_sprite->transform, rr_sprite->scale, rr_sprite->rotation, rr_sprite->position);
+}
+
+void render_simple_rr_sprite(nl_rr_sprite* const spr)
+{
+    glBindVertexArray(spr->VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, spr->VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, spr->EBO);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, spr->TextureID);
+   
+    unsigned int model_loc = get_uniform_loc(rr_shader_program, "uModelTransform");
+    set_uniform_mat4x4f(model_loc, &spr->transform.m11);
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindVertexArray(0);
+}
+
+void free_simple_rr_sprite(nl_rr_sprite* const sprite)
+{
+    glDeleteBuffers(1, &sprite->EBO);
+    glDeleteBuffers(1, &sprite->VBO);
+    glDeleteVertexArrays(1, &sprite->VAO);
+    glDeleteTextures(1, &sprite->TextureID);
+
+    sprite->EBO = sprite->VBO = sprite->VAO = sprite->TextureID = 0;
+}
---
+++ projects/rr_screenbounce/rr_screenbounce.c
@@ -0,0 +1,85 @@
+#include "nl_lib.h"
+#include "nl_rr_sprite.h"
+#include "nl_rr_camera.h"
+
+// Goal of this project: To see if the top of the screen is the same with the camera unmoving
+
+nl_rr_sprite TWO = {0};
+nl_rr_camera main_camera = {0};
+
+int _width;
+int _height;
+
+float speed = 10;
+v2f direction = {1.0f,1.0f};
+
+unsigned int rr_shader_program = 0;
+
+internal_function void winsizecbk(int width, int height)
+{
+    _width = width;
+    _height = height;
+
+    const float aspect = (float)width / (float)height;
+    main_camera.aspect_ratio = aspect;
+    main_camera.transform_dirty = 1;
+}
+
+void app_specific_init(void)
+{
+    create_simple_rr_sprite("thing.png", &TWO);
+    scale_matrix_2f(&TWO.transform, (v2f){0.5f,0.5f});
+
+    rr_shader_program = load_shader_from_files("rr_shader_matrices.vs", "rr_shader.fs");
+    use_shader_program(rr_shader_program);
+
+    initialize_rr_camera_to_default(&main_camera);
+    main_camera.position.z = 100.f;
+
+    set_window_size_callback(&winsizecbk);
+    v2i screen_size = get_screen_size();
+    winsizecbk(screen_size.x, screen_size.y);
+
+    setup_camera_matrices(&main_camera);
+}
+
+void app_specific_update(double dt)
+{
+    TWO.rotation.y += 75.f * dt;
+
+    TWO.position.x += speed * dt * direction.x;
+    TWO.position.y += speed * dt * direction.y;
+
+    if (TWO.position.x + TWO.scale.x > _width || TWO.position.x - TWO.scale.x < -_width)
+    {
+        direction.x *= -1.f;
+    }
+
+    if (TWO.position.y + TWO.scale.y > _height || TWO.position.y - TWO.scale.y < -_height)
+    {
+        direction.y *= -1.f;
+    }
+
+    create_srt_matrix(&TWO.transform, TWO.scale, TWO.rotation, TWO.position);
+
+    update_camera(&main_camera, dt);
+}
+
+void app_specific_render(void)
+{
+    use_shader_program(rr_shader_program);
+    setup_camera_matrices(&main_camera);
+    
+    render_simple_rr_sprite(&TWO);
+}
+
+void app_specific_cleanup(void)
+{
+    free_simple_rr_sprite(&TWO);
+
+    free_shader_program(rr_shader_program);
+}
+
+
+#include "nl_rr_sprite.c"
+#include "nl_rr_camera.c"
