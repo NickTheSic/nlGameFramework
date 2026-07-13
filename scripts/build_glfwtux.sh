dd01776308d4173b8acf4ef0bf074ea400142311
nick <nickthesic@gmail.com>
2026-07-12 21:20:12 -0400

    It seg faults and I am not sure why.  I think it wants a .so dynamic library and isn't loading the right one but glfw didn't create one why I built it so Idk

--- scripts/build_glfwtux.sh
+++ scripts/build_glfwtux.sh
@@ -3,9 +3,9 @@
 mkdir -p "_build/glfwtux/$1"
 pushd "_build/glfwtux/$1"
 
-libs="-lpthread -ldl -lm -L../../../nl_lib/libs -lfreetype -lglfw3"
+libs="-L../../../nl_lib/libs -lfreetype -lglfw3 -lpthread -ldl -lm"
 flags="-g -O0 --static -DNL_BUILD_GLFW"
-includes="-I../../../nl_lib -I../../../projects/$1 -I../../../nl_lib/third_party/freetype/include -I../../../nl_lib/third_party/glfw/include"
+includes="-I../../../nl_lib -I../../../projects/$1 -I../../../nl_lib/third_party/freetype/include"
 FILES="../../../nl_lib/build_nl_lib.c ../../../projects/$1/$1.c"
 
 gcc $flags $includes $FILES $libs -o $1.out
