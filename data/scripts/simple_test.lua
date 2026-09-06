1b2ce3eee3f3586fe08670bf0d15e988f7611db1
nick <nickthesic@gmail.com>
2026-09-06 16:38:15 -0400

    More basic lua testing

---
+++ data/scripts/simple_test.lua
@@ -0,0 +1,1 @@
+-- TODO: Make a simplem test file for testing lua embedding
--- projects/embed_lua/embed_lua.c
+++ projects/embed_lua/embed_lua.c
@@ -1,6 +1,19 @@
 #include "nl_lib.h"
 #include "build_lua.c"
 
+/*
+LUA TYPES:
+    nil
+    boolean
+    light userdata (* A C pointer)
+    number (a 64 bit number)
+    string (garbage collected)
+    table (complex type) { "foo", "bar" }
+    function (like lambdas)
+    userdata (your own type)
+    thread
+*/
+
 global_variable lua_State* L = 0;
 
 void app_specific_init(void)
