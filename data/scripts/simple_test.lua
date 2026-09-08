acd6895e0be8199ac968156b2406c17d87e09ad7
nick <nickthesic@gmail.com>
2026-09-08 19:20:54 -0400

    call a c function in lua!

--- data/scripts/simple_test.lua
+++ data/scripts/simple_test.lua
@@ -1,9 +1,8 @@
 -- TODO: Make a simplem test file for testing lua embedding
 
-print ('Script run')
+-- print('Simple Lua Script Executing!')
 
-print ('Second Line')
+print('lua rewritten')
 
-print ('for purposes of visual novel I must figure out how to pause, or at least stop reading when it gets to a line')
-
-print ('and if that is not possible then I am just using lua for no reason')
+myccall('hello') -- this is bound in C 
+myccall2() -- this does not exist!
--- projects/embed_lua/embed_lua.c
+++ projects/embed_lua/embed_lua.c
@@ -1,5 +1,9 @@
 #include "nl_lib.h"
-#include "build_lua.c"
+
+#include "lua/src/lua.h"
+#include "lua/src/lualib.h"
+#include "lua/src/lauxlib.h"
+
 
 /*
 LUA TYPES:
@@ -14,23 +18,45 @@ LUA TYPES:
     thread
 */
 
-global_variable lua_State* L = 0;
+static int test_lua_call(lua_State* L)
+{
+    int argc = lua_gettop(L);
+    if (argc == 0)
+    {
+        NL_LOG("No Arguments Provided");
+    }
+    else if (argc == 1)
+    {
+        NL_LOG("One Argument Provided!");
+    }
+}
 
 void app_specific_init(void)
 {
-    L = luaL_newstate(); // lua_newstate(lua Allocator) -> if I need so in the future
-    luaL_openlibs(L);
-    luaL_dostring(L, "print('Hello from Lua!')");
-
-    // Load file, should name it main.lua or something in the future for consistency
-    file_contents lua_script = {0};
-    read_entire_file("data/scripts/simple_test.lua", &lua_script, get_transient_bump_allocator());
-    luaL_dostring(L, lua_script.content);
 }
 
 void app_specific_update(double dt)
 {
     NL_UNUSED(dt);
+
+    if (key_was_pressed(key_r))
+    {
+        lua_State* L = luaL_newstate(); // lua_newstate(lua Allocator) -> if I need so in the future
+        luaL_openlibs(L);
+
+        lua_pushcfunction(L, test_lua_call);
+        lua_setglobal(L, "myccall");
+        
+        // Load file, should name it main.lua or something in the future for consistency
+        file_contents lua_script = {0};
+        read_entire_file("data/scripts/simple_test.lua", &lua_script, get_temporary_bump_allocator());
+        
+        luaL_dostring(L, lua_script.content);
+        lua_close(L);
+
+        clear_file_read(&lua_script);
+        flush_bump_allocator(get_temporary_bump_allocator());
+    }
 }
 
 void app_specific_render(void)
@@ -40,5 +66,9 @@ void app_specific_render(void)
 
 void app_specific_cleanup(void)
 {
-    lua_close(L);
+
 }
+
+
+#undef KB
+#include "build_lua.c"
